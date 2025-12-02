752516d10543ed76fa6532eccfa662a2b6298276
Daniel HP <danielhp@Mac-mini-de-Daniel.local>
2025-12-02 14:05:08 -0600

    add: Controller to login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'views/controller/login/UserModel.dart';
 import 'views/login_screen.dart';
 
 void main() {
@@ -10,14 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Rutas Busmen',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFFF5F5F7),
+    return ChangeNotifierProvider(
+      create: (context) => LoginViewModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Rutas Busmen',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: const Color(0xFFF5F5F7),
+        ),
+        home: const LoginScreen(),
       ),
-      home: const LoginScreen(),  // ← Usa tu pantalla de login
     );
   }
 }
