b37da3f9c03c0701108084cecb1006eb4aa2072b
Daniel Hernández Palagot <danielhernendez@geovoy.com>
2026-03-04 10:32:16 -0600

    add: new api key in maps

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now,
             // so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-            // signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("debug")
+             signingConfig = signingConfigs.getByName("release")
         }
     }
 }
